08535d5f0d87eaba0638b497c0413c2f46cc009a
Miles Disch <milesdisch@Miless-MacBook-Air.local>
2016-06-06 17:18:25 +1000

    "-DELETED: Cleand up some assets that weren't needed"

--- aud_vis@campjs/src/client/js/main.js
+++ aud_vis@campjs/src/client/js/main.js
@@ -1,45 +1,9 @@
-/*
-*  require all files here
-*/
+(function (window, document){
 
-var $ = require('./utils')();
-var scroll = require('./scroll');
-var Jump = require('jump.js');
+    var utils = require('utils');   
 
-(function(window, document, undefined, callback){
+    document.addEventListener('DOMContentLoaded', function(e){
+        console.log('Document is ready!');
+    })    
 
-    document.addEventListener('DOMContentLoaded', function(evt) {
-
-        //* Storing all elements *//
-        var moon = $.qSelect('.scroll');
-        var hamburger = $.qSelect('.container-hamburger');
-        var logo = $.qSelect('.container-logo');
-       
-        //* Click handlers *//
-        hamburger.onclick = function (evt) {
-            $.toggleClass(this.parentNode, 'items-visible');
-        };
-
-        logo.onclick = function (evt) {
-            $.toggleClass(this.children[0], '-flyRight');
-        };
-
-        document.onclick= function (evt) { 
-            if (evt.target.classList[0] === 'scroll') {
-                $.removeClass(moon, '-dimAndShrink');
-                $.addClass(moon, '-glowAndExpand');
-            } else {
-                $.addClass(moon, '-dimAndShrink');
-            }
-        };
-
-        var ting = new Jump();
-    });
-
-    window.onload = function () {
-        //.. handle scripts on window loaded
-        // This will fire second
-        
-        
-    };
-})(window, document, undefined, function(){});
+})(window || this, document || window.document, undefined);
