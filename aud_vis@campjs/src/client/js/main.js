709fd1bb5c3c02aeffd1f9bdad5fb956a194d89c
Miles Disch <milesdisch@Miless-MacBook-Air.local>
2016-06-07 15:21:59 +1000

    "-ADDED: Added audio context library"

--- aud_vis@campjs/src/client/js/main.js
+++ aud_vis@campjs/src/client/js/main.js
@@ -1,9 +1,12 @@
-(function (window, document){
+var utils = require('./utils.js');
 
-    var utils = require('utils');   
+(function (window, document){
 
     document.addEventListener('DOMContentLoaded', function(e){
         console.log('Document is ready!');
-    })    
+
+        var audioContext = new AudioContext();
+        
+   	});
 
 })(window || this, document || window.document, undefined);
--- aud_vis@campjs/webroot/client/js/main.js
+++ aud_vis@campjs/webroot/client/js/main.js
@@ -1,3 +1,17 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var utils = require('./utils.js');
+
+(function (window, document){
+
+    document.addEventListener('DOMContentLoaded', function(e){
+        console.log('Document is ready!');
+
+        var audioContext = new AudioContext();
+        
+   	});
+
+})(window || this, document || window.document, undefined);
+},{"./utils.js":2}],2:[function(require,module,exports){
 module.exports = function utils(){
     
     var ERR = {
@@ -86,3 +100,8 @@ module.exports = function utils(){
 
     return publicAPI;
 };
+
+},{}]},{},[1])
+
+
+//# sourceMappingURL=main.js.map
