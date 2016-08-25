dd1a5c3f02fb83a3d181316206525565b3f01dfc
Miles Disch <milesdisch@Miless-MacBook-Air.local>
2016-08-26 08:43:49 +1000

    Added tests stuff.

--- _learning/@6/main.js
+++ _learning/@6/main.js
@@ -9,7 +9,7 @@
       toEql: function (expected) {
         var result = !!(val === expected);
         if (result) {
-          console.log('='+result);
+          console.log('\n\t\t'+result);
         } else {
           throw new Error('\nexpected ' + expected + '\n' +'actual ' + val);
         }
@@ -18,26 +18,44 @@
   }
 
   function describe(description, cb) {
+
     console.log(description);
     cb();
   }
 
   function it(description, cb) {
-    console.log(description);
+    console.log('\t' + description);
     cb()
   }
 
   describe('Vector', function (){
+    beforeEach();
     describe('getX', function () {
       it('Should get 10', function () {
-        expect(FIXTURE.getX()).toEql(9);
-      })
-    })
+        expect(FIXTURE.getX()).toEql(10);
+      });
+    });
+
     describe('getY', function () {
       it('Should get 20', function () {
         expect(FIXTURE.getY()).toEql(20);
-      })
+      });
+    });
+
+    describe('setX', function () {
+      it('Should set the state to 0', function () {
+        FIXTURE.setX(0)
+        expect(FIXTURE.getX(0)).toEql(0)
+      });
+    });
+
+    describe('setY', function () {
+      it('Should set the state to 0', function () {
+        FIXTURE.setY(1)
+        expect(FIXTURE.getY()).toEql(1)
+      });
     })
+
   })
 
 })(vector)
